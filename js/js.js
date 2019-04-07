4f7e7872955c14486552a317a60c01e2c751c46f
Svift <kuryakov-s@mail.ru>
2019-04-07 18:15:09 +0700

    Второе задание

--- js/js.js
+++ js/js.js
@@ -1,19 +1,55 @@
-let money = prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
-let mandatoryExpenses = prompt('Введите обязательную статью расходов в этом месяце');
-let amountExpenses = prompt('Во сколько обойдется?');
+let money = +prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        mandatoryExpenses: amountExpenses
-    },
-    optionalExpenses: {
-
-    },
+    expenses: {},
+    optionalExpenses: {},
     income: [],
     savings: false
-}
+};
+let i = 0;
 
-console.log((+appData.budget - +appData.expenses.mandatoryExpenses) / 30);
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null 
+//     && (typeof(b)) != null && a != '' && a.length < 50) {
+//         appData.expenses[a] = b;
+//     } else {
+//         alert('Введены некоректные данные, повторите попытку еще раз');
+//     }
+// }
+
+// while (i < 2) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько обойдется?', '');
+
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null 
+//     && (typeof(b)) != null && a != '' && a.length < 50) {
+//         appData.expenses[a] = b;
+//     } else {
+//         alert('Введены некоректные данные, повторите попытку еще раз');
+//     } 
+//     i++;
+// }
+
+do {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
+
+    if ((typeof(a)) === 'string' && (typeof(a)) != null 
+    && (typeof(b)) != null && a != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    } else {
+        alert('Введены некоректные данные, повторите попытку еще раз');
+    } 
+    i++;
+} 
+while (i < 2)
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
